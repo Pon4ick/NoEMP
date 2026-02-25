dae43c447f5c6820f4a09ea4baf46b676e6720d6
pon4ick <ikonstantin2010@gmail.com>
2026-02-25 19:54:04 +0300

    init

---
+++ noemp.js
@@ -0,0 +1,59 @@
+// ==UserScript==
+// @name         NoEMP
+// @namespace    http://tampermonkey.net/
+// @version      0.0.1-beta
+// @description  NoEMP
+// @author       Pon4ick
+// @match        https://kb.bvbinfo.ru/lessons/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var isProcessed = false;
+
+    function main() {
+        let videos = document.querySelectorAll(".vjs-poster");
+        console.warn(videos)
+        for (let i = 1; i < videos.length; i++) {
+            let element = videos[i]
+            element.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.remove()
+        }
+
+        let navItems = document.querySelectorAll(".pl-2.mb-1.text-sm");
+        let videoText = [];
+        for (let element of navItems) {
+            if (element.children[0].innerText.toLowerCase().includes("видео")) {
+                videoText.push(element)
+            }
+        }
+        for (let i = 1; i < videoText.length; i++) {
+            let element = videoText[i]
+            element.remove()
+        }
+    }
+
+
+    function doYourStuff() {
+        let navItems = document.querySelectorAll(".vjs-poster");
+        if (navItems.length > 0) {
+            main()
+            return true;
+        }
+        return false;
+    }
+
+    if (!doYourStuff()) {
+
+        const observer = new MutationObserver(function(mutations) {
+            if (doYourStuff()) {
+                //observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    }
+})();
